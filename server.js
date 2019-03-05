d513e0d749ef96dc290363f30367e67088eeeb26
Temi Situ-Amoo <36303922+Und3rc0v3r@users.noreply.github.com>
2019-03-05 13:51:53 +0000

    Fixing dropdown
    dropdown fixes, also linking core bootstrap at bottom of index page

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require("express");
 var recipes = require("./recipies.json");
 var app = express();
 
+var itemTypes = [{"type":"Tops"},{"type":"Jeans"},{"type":"Tracksuits"},{"type":"Jumpers"}]
+
+
 app.get("/", function (req, resp) {
 	resp.send("Hello world");
 });
@@ -9,5 +12,9 @@ app.get("/", function (req, resp) {
 app.get("/food", function (req, resp) {
 	resp.send(recipes);
 });
+
+app.get("/itemtypes", function (req, resp) {
+	resp.send(itemTypes);
+})
 
 app.listen(8090);

d500acbcd256522ab3b9389e389984164e1bb0ea
Und3rc0v3r <peter.t.situ-amoo@durham.ac.uk>
2019-05-02 12:20:55 +0100

    cors hotfix

--- app.js
+++ app.js
@@ -15,6 +15,14 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("client"));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 /**
  * Adding clothing data
  * @type {JSON} 
